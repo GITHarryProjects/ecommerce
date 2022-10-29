e7d0707cb0ce2a45845271a2700dd80a69ddf2b7
Harry <harald.innetzberger@web.de>
2022-10-29 13:50:50 +0200

    progress

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,21 +1,24 @@
 import Link from 'next/link'
 import React from 'react'
 
-export const HeroBanner = () => {
+import { urlFor } from '../lib/client'
+
+export const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">SMALL TEXT</p>
-        <h3>MID Text</h3>
-        <img src="" alt="berries" className="hero-banner-image" />
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img src={urlFor(heroBanner.image)} alt="berries" className="hero-banner-image" />
 
         <div>
-          <Link href="/products/ID">
-            <button type="button">BUTTON TEXT</button>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
             <h5>Description</h5>
-            <p>DESCRIPTION</p>
+            <p>{heroBanner.desc}</p>
           </div>
         </div>
       </div>
